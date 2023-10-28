202fac149886f4f712caa0ba2bc0f29807494bc8
Diptajit <93245103+diptajit1996@users.noreply.github.com>
2023-10-28 12:43:12 +0530

    Files Upload

---
+++ registration/app1/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+@login_required(login_url='login')
+def HomePage(request):
+    return render(request, 'home.html')
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Your password and confirm password are not same!!")
+        else:
+            # print(uname, email, pass1, pass2)
+
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+
+            # return HttpResponse("User has been created successfully!!!")
+
+            return redirect('login')
+
+    return render(request, 'signup.html')
+
+def LoginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+
+        # print(username, pass1)
+
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse("Username and password is incorrect!!")
+
+    return render(request, 'login.html')
+
+def BasePage(request):
+    return render(request, 'base.html')
+
+def ServicesPage(request):
+    return render(request, 'services.html')
+
+def ContactPage(request):
+    return render(request, 'contact.html')
+
+def AboutPage(request):
+    return render(request, 'about.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
